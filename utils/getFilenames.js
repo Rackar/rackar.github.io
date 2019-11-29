780d48d5513f6b70ae255ecf774820073f077da1
旭 杨 <27681135@qq.com>
2019-11-30 00:06:06 +0800

    升级命令

--- utils/getFilenames.js
+++ utils/getFilenames.js
@@ -1,14 +1,10 @@
-const {readdir, readFile, writeFile} = require("fs");
+const { readdir, readFile, writeFile, readdirSync, statSync } = require("fs");
+let path = require("path");
 // const {resolve} = require('path')
 
 // const FOLDERPATH = './docs/article'
-const PathsIn = [
-  "./docs/article_child/",
-  "./docs/article_self/",
-  "./docs/article_tech/"
-];
 
-const PathsInNew = ["/article_child/", "/article_self/", "/article_tech/"];
+const pathsIgnore = [".vuepress", "en", "pic"];
 
 function pReadFile(filepath) {
   return new Promise((resolve, reject) => {
@@ -29,13 +25,38 @@ function pReadFile(filepath) {
     });
   });
 }
+function getAllDirs(mypath = "docs") {
+  const items = readdirSync(mypath);
 
+  let result = [];
+
+  // 遍历当前目录中所有的文件和文件夹
+
+  items.map(item => {
+    let temp = path.join(mypath, item);
+
+    // 若当前的为文件夹
+
+    if (statSync(temp).isDirectory()) {
+      if (
+        pathsIgnore.every(tt => {
+          return tt != item;
+        })
+      ) {
+        result.push(item);
+      }
+    }
+  });
+  console.log(result);
+  return result;
+}
 async function doAllJobs(params) {
   var params = {};
+  let PathsInNew = getAllDirs();
   for (let index = 0; index < PathsInNew.length; index++) {
     const dir = PathsInNew[index];
     console.log(dir);
-    let fileNames = await pReadFile("./docs" + dir);
+    let fileNames = await pReadFile("./docs/" + dir);
     params[dir] = fileNames;
   }
   console.log(params);
@@ -43,23 +64,6 @@ async function doAllJobs(params) {
 }
 
 doAllJobs();
-
-// Promise.all([
-//   pReadFile(PathsIn[0]),
-//   pReadFile(PathsIn[1]),
-//   pReadFile(PathsIn[2])
-// ]).then(
-//   arr => {
-//     console.log(arr);
-//     var params = {
-//       "/article_child/": arr[0],
-//       "/article_self/": arr[1],
-//       "/article_tech/": arr[2]
-//     };
-//     writeJson(params);
-//   },
-//   err => console.log(err)
-// );
 
 function writeJson(params) {
   //现将json文件读出来
@@ -72,7 +76,7 @@ function writeJson(params) {
     person = JSON.parse(person); //将字符串转换为json对象
     // person['/article_child/'] = params['/article_child/']
     for (let key in params) {
-      person[key] = params[key];
+      person[`/${key}/`] = params[key];
     }
     // console.log(person);
     var str = JSON.stringify(person, null, 2); //因为nodejs的写入文件只认识字符串或者二进制数，所以把json对象转换成字符串重新写入json文件中
