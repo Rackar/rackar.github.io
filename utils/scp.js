0952b9fcc0ac248e357895057365d4bbcab0a58f
rackar <27681135@qq.com>
2020-03-22 18:59:41 +0800

    添加CI脚本

---
+++ utils/scp.js
@@ -0,0 +1,34 @@
+let client = require("scp2");
+let { secret } = require("./secret");
+
+//
+// secret.js 格式
+//
+// const secret = {
+//   host: "xx.xxx.xxx.xx",
+//   username: "root",
+//   password: "password",
+//   path: "/root/myapp/www/"
+// };
+//
+// exports.secret = secret;
+//
+
+//scp覆盖上传整个目录
+client.scp(
+  "docs/.vuepress/dist/",
+  // "2.txt",
+  {
+    host: secret.host,
+    username: secret.username,
+    password: secret.password,
+    path: secret.path
+  },
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("blog update success");
+    }
+  }
+);
