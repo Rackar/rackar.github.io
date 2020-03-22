c2444acd50cfc347d3b22c8106b95f7be0b69ed5
rackar <27681135@qq.com>
2020-03-23 01:53:27 +0800

    百度统计代码修正

--- utils/bd_hm.js
+++ utils/bd_hm.js
@@ -26,14 +26,14 @@ var baidu = `<script>
 
 function writeJson() {
   //现将json文件读出来
-  fs.readFile('./docs/.vuepress/dist/index.html', function (err, data) {
+  fs.readFile('./public/index.html', function (err, data) {
     if (err) {
       return console.error(err)
     }
     var person = data.toString() //将二进制的数据转换为字符串
     var pos = person.indexOf('<meta charset="utf-8">')
     person = person.slice(0, pos) + baidu + person.slice(pos)
-    fs.writeFile('./docs/.vuepress/dist/index.html', person, function (err) {
+    fs.writeFile('./public/index.html', person, function (err) {
       if (err) {
         console.error(err)
       }
